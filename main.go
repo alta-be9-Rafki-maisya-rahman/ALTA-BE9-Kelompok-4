a3fb31388d88d81345c4bc3adef68b7468f602ce
FaizalSundara <faizalramadhansundara@gmail.com>
2022-06-09 08:02:52 +0700

    fitur melihat profil pengguna lain

--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import (
 	_config "be9/app-project/config"
 	_topUpBalanceController "be9/app-project/controllers/topupbalance"
+	_user "be9/app-project/controllers/user"
 
 	// _transferController "be9/app-project/controllers/transfer"
 
-	// _entities "be9/app-project/entities"
+	_entities "be9/app-project/entities"
 	"database/sql"
 	"fmt"
 )
@@ -19,32 +20,41 @@ func init() {
 
 func main() {
 	var pilihan int
-	fmt.Println("Pilih menu berikut: (1:Top Up) / // (2:Cek History Top Up) / (3: Transfer) / (4: Cek History Transfer) / (5 : Keluar)")
+	fmt.Println("Pilih menu berikut: (1:Top Up) / // (2:Cek History Top Up) / (3: Lihat Pengguna Lain) / (4: Cek History Transfer) / (5 : Keluar)")
 	fmt.Scanln(&pilihan)
 	switch pilihan {
 	case 1:
 		//Feature Top Up
-		// newTopUpBalance := _entities.TopUpBalance{}
-		// fmt.Println("Input Telp:")
-		// fmt.Scanln(&newTopUpBalance.Telp)
-		// fmt.Println("Input Nominal:")
-		// fmt.Scanln(&newTopUpBalance.NominalTopUp)
+		newTopUpBalance := _entities.TopUpBalance{}
+		fmt.Println("Input Telp:")
+		fmt.Scanln(&newTopUpBalance.Telp)
+		fmt.Println("Input Nominal:")
+		fmt.Scanln(&newTopUpBalance.NominalTopUp)
 
-		// row, err := _topUpBalanceController.CreateTopUpBalance(DBconn, newTopUpBalance)
-		// if err != nil {
-		// 	fmt.Println("error insert", err.Error())
-		// } else {
-		// 	fmt.Println("Insert Success")
-		// 	fmt.Println("row affect", row)
-		// }
+		row, err := _topUpBalanceController.CreateTopUpBalance(DBconn, newTopUpBalance)
+		if err != nil {
+			fmt.Println("error insert", err.Error())
+		} else {
+			fmt.Println("Insert Success")
+			fmt.Println("row affect", row)
+		}
 	case 2:
-		var cekTopUp string
+		var cekTopUp1 string
 		fmt.Println("input Telp:")
-		fmt.Scanln(&cekTopUp)
+		fmt.Scanln(&cekTopUp1)
 
-		result := _topUpBalanceController.GetHistoryTopUp(DBconn, cekTopUp)
+		result := _topUpBalanceController.GetHistoryTopUp(DBconn, cekTopUp1)
 		for _, v := range result {
 			fmt.Println("ID:", v.ID, "Nominal Top Up:", v.NominalTopUp, "Tanggal:", v.Tanggal, "Telp:", v.Telp)
+		}
+	case 3:
+		searchOtherUser := ""
+		fmt.Print("Masukkan Telp:")
+		fmt.Scanln(&searchOtherUser)
+
+		row := _user.SearchUser(DBconn, searchOtherUser)
+		for _, v := range row {
+			fmt.Println("ID:", v.ID, "Nama:", v.Nama, "Telp:", v.Telp)
 		}
 	}
 
