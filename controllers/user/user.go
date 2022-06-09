542b5258c8e8638d3199436ca9a7b51688d79f67
rafki maisya <rafkiadb@gmail.com>
2022-06-09 11:24:48 +0700

    fitur add user tidak bisa kosong

--- controllers/user/user.go
+++ controllers/user/user.go
@@ -3,16 +3,21 @@ package user
 import (
 	_entities "be9/app-project/entities"
 	"database/sql"
+	"fmt"
 )
 
 func AddAccount(db *sql.DB, newAccount _entities.User) (int, error) {
-	var query = ("INSERT INTO user(id, user_name, telp, password) VALUES(?,?,?,?)")
+
+	if newAccount.Nama == "" || newAccount.Password == "" || newAccount.Telp == "" {
+		return 0, fmt.Errorf(" Data tidak boleh Kosong")
+	}
+	var query = ("INSERT INTO user(user_name, telp, password) VALUES(?,?,?)")
 	statement, errPrepare := db.Prepare(query)
 	if errPrepare != nil {
 		return 0, errPrepare
 	}
 
-	result, err := statement.Exec(newAccount.ID, newAccount.Nama, newAccount.Telp, newAccount.Password)
+	result, err := statement.Exec(newAccount.Nama, newAccount.Telp, newAccount.Password)
 
 	defer db.Close()
 
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_config "be9/app-project/config"
+	_login "be9/app-project/controllers/login"
 	_controllers "be9/app-project/controllers/user"
 	_entities "be9/app-project/entities"
 	"database/sql"
@@ -15,15 +16,15 @@ func init() {
 }
 
 func main() {
-	fmt.Println("Pilih Menu: (1: Register Account)")
+	fmt.Println("Pilih Menu: (1: Register Account) (2: Login)")
 	var pilihan int
 	fmt.Scanln(&pilihan)
+	defer DBconn.Close()
 
 	switch pilihan {
 	case 1:
 		newAccount := _entities.User{}
-		fmt.Println("Input ID")
-		fmt.Scanln(&newAccount.ID)
+
 		fmt.Println("Input NAMA")
 		fmt.Scanln(&newAccount.Nama)
 		fmt.Println("Input Password")
@@ -38,6 +39,28 @@ func main() {
 		} else {
 			fmt.Println("registration succes")
 			fmt.Println("row affect", row)
+		}
+
+	case 2:
+		loginUser := _entities.User{}
+		fmt.Println("Input Telp")
+		fmt.Scanln(&loginUser.Telp)
+		fmt.Println("Input Password")
+		fmt.Scanln(&loginUser.Password)
+
+		row, err := _login.UserLogin(DBconn, loginUser)
+
+		if err != nil {
+			fmt.Println("login failed", err.Error(), "data tidak cocok")
+		} else {
+			fmt.Println("login succes")
+			fmt.Println("row affect", row)
+			var userMenu int
+			fmt.Scanln(&userMenu)
+			switch userMenu {
+			case 1:
+
+			}
 		}
 
 	}
